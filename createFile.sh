b64a8cd3b072d664b2d4c821b24386b26bdb8c5e
TrianguloY <correo--correo@hotmail.com>
2019-10-05 13:48:56 +0200

    Simplified file location

--- createFile.sh
+++ createFile.sh
@@ -1,7 +1,7 @@
 zip translation_files.zip \
 	README.md \
 	PlayStore/README.md \
-	PlayStore/en_gb/text.xml \
+	PlayStore/en_gb.xml \
 	res/README.md \
 	res/values/strings.xml \
 	res/raw/countries_name.txt

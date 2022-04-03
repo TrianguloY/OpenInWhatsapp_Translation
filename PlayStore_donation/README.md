645b129a8b488b643a0b73fbcd64a87b1373e315
TrianguloY <correo--correo@hotmail.com>
2022-04-03 15:24:33 +0200

    Added Play Store donation strings

---
+++ PlayStore_donation/README.md
@@ -0,0 +1,7 @@
+# Play Store donation folder
+
+PlayStore's strings for the donation version.
+
+Important! You don't need to translate this if you didn't give permission to publish your translation in the donation version.
+
+Also remember to make sure you keep the number of characters under the maximum specified!!! Otherwise I won't be able to publish it.
--- updateZip.sh
+++ updateZip.sh
@@ -1,10 +1,12 @@
-zip translation_files.zip      \
-	README.md                  \
-	PlayStore/README.md        \
-	PlayStore/en-GB.xml        \
-	res/README.md              \
-	res/values/strings.xml     \
-	res/raw/countries_name.txt \
+zip translation_files.zip        \
+	README.md                    \
+	PlayStore/README.md          \
+	PlayStore/en-GB.xml          \
+	PlayStore_donation/README.md \
+	PlayStore_donation/en-GB.xml \
+	res/README.md                \
+	res/values/strings.xml       \
+	res/raw/countries_name.txt   \
 	res/raw/changelog.txt
 	
 echo "updated translation_files.zip"
